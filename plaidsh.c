8e3a8e3d9f99e69c58ec36007493e9fe278cf743
uwasepauline7 <puwase@andrew.cmu.edu>
2024-12-07 21:25:35 +0200

    creating a token

--- plaidsh.c
+++ plaidsh.c
@@ -1,50 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
-#include <readline/history.h>
+#include <readline/history.h>  
 
 #include "clist.h"
 #include "Token.h"
-#include "Tokenize.h"
+#include "Tokenize.h"  
 
 int main() {
     printf("Welcome to Plaid Shell!\n");
     CList tokens = NULL;
-    char errmsg[256];
+    char errmsg[256];      
 
     while (1) {
         // Display the prompt
-        char *input = readline("#? ");
+        char *input = readline("#? ");          
 
         // Check for EOF (Ctrl+D)
         if (!input) {
             printf("\nExiting Plaid Shell. Goodbye!\n");
             break;
-        }
+        }          
 
         // Add the command to history
         if (*input) {
             add_history(input);
-        }
+        }          
 
-        tokens = TOK_tokenize_input(input, errmsg, sizeof(errmsg));
-
+        tokens = TOK_tokenize_input(input, errmsg, sizeof(errmsg));          
         if(tokens == NULL) {
             fprintf(stderr, "%s\n", errmsg);
-            goto loop_end;
+            free(input);  // Free input on tokenization failure
+            continue;  // Go to next iteration
         }
-        TOK_print(tokens);
+
+        TOK_print(tokens);          
 
         // Free the allocated memory
         free(input);
-
-        loop_end:
-          free(input);
-          input = NULL;
-          CL_free(tokens);
-          tokens = NULL;
-
-    }
+        CL_free(tokens);
+        tokens = NULL;
+    }      
 
     return 0;
 }
