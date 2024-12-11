b1d98b562da2c081245106d379f5b5f33edba9f4
uwasepauline7 <puwase@andrew.cmu.edu>
2024-12-11 17:34:35 +0200

    making changes

--- parse.h
+++ parse.h
@@ -2,22 +2,7 @@
 #define PARSE_H
 
 #include "Tokenize.h"
-#include "pipeline.h"
-
-
-// Representation of a single command
-typedef struct Command
-{
-    char *command;   // The command (e.g., "cat")
-    CList arguments; // List of arguments (e.g., ["file1.txt"])
-} Command;
-
-// Representation of the entire pipeline
-typedef struct Pipeline
-{
-    Command *command;      // A command
-    struct Pipeline *next; // Next command in the pipeline
-} Pipeline;
+#include "ast.h"  // Include ast.h to use the Command and Pipeline structs
 
 // Function to parse a list of tokens into a pipeline
 Pipeline *parse_tokens(CList tokens, char *errmsg, size_t errmsg_sz);
@@ -25,4 +10,4 @@ Pipeline *parse_tokens(CList tokens, char *errmsg, size_t errmsg_sz);
 // Helper functions
 void free_pipeline(Pipeline *pipeline);
 
-#endif
+#endif // PARSE_H
